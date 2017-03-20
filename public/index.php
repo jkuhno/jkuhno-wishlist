e6d2de693a5429866ff561f665d3107f777ebfcb
jkuhno <jani@Jani-MacBook-Pro.local>
2017-03-20 20:08:53 +0200

    test

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-    require 'core/bootstrap.php';
+    require '/core/bootstrap.php';
 
-    require Router::define(require 'app/routes.php')
+    require Router::define(require '/app/routes.php')
         ->fire(Request::uri());
