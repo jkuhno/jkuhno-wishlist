cd8e25f6a2e963fa183c9865307af3f11812ffcd
jkuhno <jani@Jani-MacBook-Pro.local>
2017-03-20 14:22:32 +0200

    Correct name
    - no need for pgsql:host=

--- core/config.php
+++ core/config.php
@@ -1,14 +1,11 @@
 <?php
 $dbopts = parse_url(getenv('DATABASE_URL'));
-/*'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
-'pdo.username' => $dbopts["user"],
-'pdo.password' => $dbopts["pass"]*/
 return [
     'database' => [
-        'name' => 'pgsql:dbname='.ltrim($dbopts["path"],'/'),
+        'name' => ltrim($dbopts["path"],'/'),
         'username' => $dbopts["user"],
         'password' => $dbopts["pass"],
-        'connection' => 'pgsql:host='$dbopts["host"] . ';port=' . $dbopts["port"],
+        'connection' => 'pgsql:host='.$dbopts["host"].';port='.$dbopts["port"],
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ]
