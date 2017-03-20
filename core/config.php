72de55cf1d70b4d6e6d6401f4585df747f3d2020
jkuhno <jani@Jani-MacBook-Pro.local>
2017-03-20 13:43:18 +0200

    Connection update
    - for heroku

--- core/config.php
+++ core/config.php
@@ -1,10 +1,14 @@
 <?php
+$dbopts = parse_url(getenv('DATABASE_URL'));
+/*'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
+'pdo.username' => $dbopts["user"],
+'pdo.password' => $dbopts["pass"]*/
 return [
     'database' => [
-        'name' => 'games',
-        'username' => 'homestead',
-        'password' => 'secret',
-        'connection' => 'mysql:host=127.0.0.1',
+        'name' => 'pgsql:dbname='.ltrim($dbopts["path"],'/'),
+        'username' => $dbopts["user"],
+        'password' => $dbopts["pass"],
+        'connection' => 'pgsql:host='$dbopts["host"] . ';port=' . $dbopts["port"],
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ]
