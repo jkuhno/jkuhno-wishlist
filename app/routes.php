9be658e74c818520f497e579f9909d009bbee509
jkuhno <jani@Jani-MacBook-Pro.local>
2017-03-21 00:48:02 +0200

    test

--- app/routes.php
+++ app/routes.php
@@ -1,7 +1,7 @@
 <?php
 $router->get('/', 'UserController@index');
 
-$router->get('/games', 'GamesController@index');
+/*$router->get('/games', 'GamesController@index');
 $router->post('/games/create', 'GamesController@create');
 $router->post('/games/delete', 'GamesController@delete');
-$router->post('/games/update', 'GamesController@update');
+$router->post('/games/update', 'GamesController@update');*/
