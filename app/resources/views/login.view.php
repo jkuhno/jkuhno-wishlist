f08ccbac5dabfe0eee0ac1db22fef7679ae99bef
jkuhno <jani@Jani-MacBook-Pro.local>
2017-03-22 12:21:43 +0200

    revert

--- app/resources/views/login.view.php
+++ app/resources/views/login.view.php
@@ -6,11 +6,11 @@
         </div>
         <?php endif; ?>
         <form action="<?= url('/login') ?>" method="POST">
-            <div class="form-group col-xs-4">
+            <div class="form-group">
                 <label for="email" class="label">Email:</label>
                 <input id="email" class="form-control" type="email" name="email" placeholder="Enter email">
             </div>
-            <div class="form-group col-xs-4">
+            <div class="form-group">
                 <label for="pwd" class="label">Password:</label>
                 <input id="pwd" class="form-control" type="password" name="password" placeholder="Enter password">
             </div>
