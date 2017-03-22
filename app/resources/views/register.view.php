1f7672e02b3416eb5e6d240cb969c326420e621f
jkuhno <jani@Jani-MacBook-Pro.local>
2017-03-22 18:56:12 +0200

    fixing

--- app/resources/views/register.view.php
+++ app/resources/views/register.view.php
@@ -17,15 +17,15 @@
         <div class="center-block register-form">
             <form action="<?= url('/register') ?>" method="POST">
                 <div class="input-group">
-                    <span class="glyphicon glyphicon-user"></i></span>
+                    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
                     <input id="name" class="form-control" type="text" name="name" placeholder="Enter your name">
                 </div>
                 <div class="input-group">
-                    <span class="glyphicon glyphicon-envelope"></i></span>
+                    <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
                     <input id="email" class="form-control" type="email" name="email" placeholder="Enter email">
                 </div>
                 <div class="input-group">
-                    <span class="glyphicon glyphicon-lock"></i></span>
+                    <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
                     <input id="pwd" class="form-control" type="password" name="password" placeholder="Enter password">
                 </div>
                 <div class="text-center">
