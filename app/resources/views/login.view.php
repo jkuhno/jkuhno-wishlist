4082ae050cc737c1e7edbfd48729682441760954
jkuhno <jani@Jani-MacBook-Pro.local>
2017-03-22 12:40:24 +0200

    css

--- app/resources/views/login.view.php
+++ app/resources/views/login.view.php
@@ -5,7 +5,7 @@
         <?= $message ?>
         </div>
         <?php endif; ?>
-        <div class="center-block">
+        <div id="login-form" class="center-block">
             <form action="<?= url('/login') ?>" method="POST">
                 <div class="form-group">
                     <label for="email" class="label">Email:</label>
