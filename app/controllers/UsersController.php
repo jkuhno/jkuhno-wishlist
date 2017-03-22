b0b664fdacb6e7e7146e797591cf63b2cf5ae6b2
jkuhno <jani@Jani-MacBook-Pro.local>
2017-03-22 13:22:16 +0200

    fixing

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -21,9 +21,9 @@ class UsersController
             $_SESSION['user_id'] = $user->id;
             header('Location: /games');
         }
-        return view("login", ["message" => "The email or password was invalid"]);
-        //$_SESSION['message'] = "The email or password was invalid!";
-        //header('Location: /');
+        //return view("login", ["message" => "The email or password was invalid"]);
+        $_SESSION['message'] = "The email or password was invalid!";
+        return header('Location: /');
     }
     public function logout()
     {
@@ -31,6 +31,6 @@ class UsersController
         session_destroy();
         //return view("login", ["message" => "Session closed"]);
         $_SESSION['message'] = "Succesfully logged out!";
-        header('Location: /');
+        return header('Location: /');
     }
 }
