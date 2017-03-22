42267c6e387a6fa3f88c6f3017d55ebdeaf00176
jkuhno <jani@Jani-MacBook-Pro.local>
2017-03-22 12:36:42 +0200

    fixing

--- app/resources/views/login.view.php
+++ app/resources/views/login.view.php
@@ -5,16 +5,18 @@
         <?= $message ?>
         </div>
         <?php endif; ?>
-        <form action="<?= url('/login') ?>" method="POST">
-            <div class="form-group">
-                <label for="email" class="label">Email:</label>
-                <input id="email" class="form-control" type="email" name="email" placeholder="Enter email">
-            </div>
-            <div class="form-group">
-                <label for="pwd" class="label">Password:</label>
-                <input id="pwd" class="form-control" type="password" name="password" placeholder="Enter password">
-            </div>
-            <button type="submit" class="btn btn-default">Log in</button>
-        </form>
+        <div class="center-block">
+            <form action="<?= url('/login') ?>" method="POST">
+                <div class="form-group">
+                    <label for="email" class="label">Email:</label>
+                    <input id="email" class="form-control" type="email" name="email" placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                    <label for="pwd" class="label">Password:</label>
+                    <input id="pwd" class="form-control" type="password" name="password" placeholder="Enter password">
+                </div>
+                <button type="submit" class="btn btn-default">Log in</button>
+            </form>
+        <div>
     </div>
 <?php require "_footer.view.php";?>
