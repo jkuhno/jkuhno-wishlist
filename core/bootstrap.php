12607118ec16cc2cd30d5ae656c1015187b3f7aa
jkuhno <jani@Jani-MacBook-Pro.local>
2017-03-23 14:59:04 +0200

    debugg off

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
-// For debugging.
+/* For debugging.
 error_reporting(E_ALL);
-ini_set('display_errors', 1);
+ini_set('display_errors', 1);*/
 
 use Wishlist\Core\App;
 use Wishlist\Core\Database\QueryBuilder;
