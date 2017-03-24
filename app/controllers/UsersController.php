a83e897f52661822eae78cbc5ac5d5542be52fb5
jkuhno <jani@Jani-MacBook-Pro.local>
2017-03-24 15:55:21 +0200

    csrf

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -42,7 +42,7 @@ class UsersController
         $token= bin2hex(openssl_random_pseudo_bytes(32));
         $_SESSION['token'] = $token;
         $users = User::findAllWhere('group_id', 2);
-        return view('admin', compact('users'));
+        return view('admin', compact('users', 'token'));
     }
     public function delete()
     {
