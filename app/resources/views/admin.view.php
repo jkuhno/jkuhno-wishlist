c5a00d982e37a77ecc7608afa3e3790ff66f3e6b
jkuhno <jani@Jani-MacBook-Pro.local>
2017-03-24 15:49:25 +0200

    delete

--- app/resources/views/admin.view.php
+++ app/resources/views/admin.view.php
@@ -3,26 +3,10 @@
         <div class="center-block well">
             <?php foreach($users as $user) :?>
                 <ul class="list-group">
-                    <li class="list-group-item"><?= htmlspecialchars($user->name); ?> | <?= htmlspecialchars($user->email); ?> | <?= htmlspecialchars($user->group_id) ?></li>
+                    <li class="list-group-item"><?= htmlspecialchars($user->name); ?> | <?= htmlspecialchars($user->email); ?> | <?= htmlspecialchars($user->group_id) ?><form action="<?= url('/admin/delete') ?>" method="POST"><input type="hidden" name="token" value="<?= $token; ?>"><input type="hidden" name="id" value="<?= $users->id; ?>"><button type="submit" class="btn btn-link">Remove</button>
+                    </form></li>
                 </ul>
             <?php endforeach; ?>
-            <!--<form action="<?= url('/register') ?>" method="POST">
-                <div class="input-group">
-                    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                    <input id="name" class="form-control" type="text" name="name" placeholder="Enter your name">
-                </div>
-                <div class="input-group">
-                    <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
-                    <input id="email" class="form-control" type="email" name="email" placeholder="Enter email">
-                </div>
-                <div class="input-group">
-                    <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                    <input id="pwd" class="form-control" type="password" name="password" placeholder="Enter password">
-                </div>
-                <div class="text-center">
-                    <button class="btn btn-default">Register</button>
-                </div>
-            </form>-->
         </div>
     </div>
 <?php require "_footer.view.php"; ?>
