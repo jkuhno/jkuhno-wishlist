a52c385f242a56a5ee903e2c7ee3d6ca4635c53f
jkuhno <jani@Jani-MacBook-Pro.local>
2017-03-26 17:05:46 +0300

    styling

--- app/resources/views/user.view.php
+++ app/resources/views/user.view.php
@@ -5,25 +5,25 @@
             <p>Name: <?= $_SESSION['name']; ?></p>
             <p>Email: <?= $_SESSION['email']; ?></p>
             <h3>Update info</h3>
-            <p class="info">Enter new value to the field you want to update.</p>
+            <p class="bg-info">Enter a new value to the field(s) you want to update.</p>
             <form class="form" action="<?= url('/user/update') ?>" method="POST">
                 <input type="hidden" name="token" value="<?= $token; ?>">
                 <input type="hidden" name="id" value="<?= $_SESSION['user_id']; ?>">
                 <div class="form-group">
-                    <label class="sr-only">Name</label>
+                    <label class="sr-only col-xs-4">Name</label>
                     <div class="input-group">
                         <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
                         <input id="updatename" class="form-control" type="text" name="name" placeholder="New name">
                     </div>
                 </div>
-                <div class="form-group">
+                <div class="form-group col-xs-4">
                     <label class="sr-only">Email</label>
                     <div class="input-group">
                         <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
                         <input id="updateemail" class="form-control" type="email" name="email" placeholder="New email">
                     </div>
                 </div>
-                <div class="form-group">
+                <div class="form-group col-xs-4">
                     <label class="sr-only">Password</label>
                     <div class="input-group">
                         <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
@@ -33,7 +33,7 @@
                 <button class="btn btn-default">Update</button>
             </form>
             <h3>Delete your account</h3>
-            <p class="warning">THIS WILL PERMANENTLY DELETE YOUR ACCOUNT AND ALL GAMES ASSOCIATED TO IT!</p>
+            <p class="bg-danger">THIS WILL PERMANENTLY DELETE YOUR ACCOUNT AND ALL GAMES ASSOCIATED TO IT!</p>
             <form class="form-inline" action="<?= url('/user/delete') ?>" method="POST">
                 <input type="hidden" name="token" value="<?= $token; ?>">
                 <input type="hidden" name="id" value="<?= $_SESSION['user_id']; ?>">
