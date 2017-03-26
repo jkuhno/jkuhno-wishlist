5c22600f7b8a0d26e7fd9294ff2cf1ca88107163
jkuhno <j.kuhno@gmail.com>
2017-03-26 20:59:12 +0300

    Update config.php

--- core/config.php
+++ core/config.php
@@ -10,9 +10,5 @@ return [
 			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 		]
 	],
-<<<<<<< HEAD
     'beginning' => 'https://jkuhno-wishlist.herokuapp.com' //https://jkuhno-wishlist-experimental.herokuapp.com
-=======
-    'beginning' => 'https://jkuhno-wishlist-experimental.herokuapp.com' //https://jkuhno-wishlist.herokuapp.com
->>>>>>> experimental
 ];
