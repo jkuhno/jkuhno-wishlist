e5a4a6ade49a0cf7f1d935089e16fdc57d69e31d
jkuhno <jani@Jani-MacBook-Pro.local>
2017-03-28 13:27:16 +0300

    asd

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+session_start();
+
+require 'vendor/autoload.php';
+
+use Wishlist\Core\Router;
+use Wishlist\Core\App;
+
+Router::define('app/routes.php')
+	->fire(
+		App::get('request')->getPathInfo(),
+		App::get('request')->getMethod()
+	);
