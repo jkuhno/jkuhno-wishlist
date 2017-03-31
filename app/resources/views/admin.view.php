ca40f99f81cc9e022e12649f4032f78ed2708734
jkuhno <jani@Jani-MacBook-Pro.local>
2017-03-31 18:29:37 +0300

    fixing

--- app/resources/views/admin.view.php
+++ app/resources/views/admin.view.php
@@ -23,32 +23,6 @@
                     <?php endforeach; ?>
                 </div>
             <?php endforeach; ?>
-            <!--<div class="table-responsive">
-                <table class="table table-condensed">
-                    <?php foreach($users as $user): ?>
-                        <thead>
-                            <tr class="info">
-                                <th><?= htmlspecialchars($user->id); ?></th>
-                                <th><?= htmlspecialchars($user->name); ?></th>
-                                <th><?= htmlspecialchars($user->email); ?></th>
-                                <th><form action="<?= url('/user/delete') ?>" method="POST"><input type="hidden" name="token" value="<?= $token; ?>"><input type="hidden" name="id" value="<?= $user->id; ?>"><input type="submit" class="btn btn-link" value="Remove"></form></th>
-                            </tr>
-                        <thead>
-                        <?php foreach($games as $game): ?>
-                            <?php if($game->user_id == $user->id): ?>
-                                <tbody>
-                                    <tr>
-                                        <td><?= htmlspecialchars($game->id); ?></td>
-                                        <td><?php if(!is_null($game->name)):?><?= htmlspecialchars($game->name); ?><?php endif;?></td>
-                                        <td><?php if(!is_null($game->releasedate)):?><?= date_format(date_create(htmlspecialchars($game->releasedate)), "F d, Y"); ?><?php endif;?></td> 
-                                        <td><form action="<?= url('/games/delete') ?>" method="POST"><input type="hidden" name="token" value="<?= $token; ?>"><input type="hidden" name="id" value="<?= $game->id; ?>"><input type="hidden" name="user_id" value="<?= $user->id; ?>"><button type="submit" class="btn btn-link">Remove</button></form></td>
-                                    </tr>
-                                </tbody>
-                            <?php endif; ?>
-                        <?php endforeach; ?>
-                    <?php endforeach; ?>
-                </table>
-            </div>-->
             <h3>Update user</h3>
             <form class="form-inline" action="<?= url('/user/update') ?>" method="POST">
                 <input type="hidden" name="token" value="<?= $token; ?>">
@@ -56,7 +30,12 @@
                     <label class="sr-only">Id</label>
                     <div class="input-group">
                         <span class="input-group-addon"><i class="glyphicon glyphicon-usd"></i></span>
-                        <input id="updateid" class="form-control" type="number" name="id" placeholder="Enter ID">
+                        <!--<input id="updateid" class="form-control" type="number" name="id" placeholder="Enter ID">-->
+                        <select class="form-control" name="id">
+                            <?php foreach($users as $user): ?>
+                                <option value="<?= $user->id; ?>"><?= htmlspecialchars($user->name); ?></option>
+                            <?php endforeach; ?>
+                        </select>
                     </div>
                 </div>
                 <div class="form-group">
